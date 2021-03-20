bfd46c48c523c00b1c813586fb266a8c7e88b56c
Tyler <tyler@programmer.net>
2021-03-19 20:58:49 -0400

    Pod Model

---
+++ app/Models/Pod.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pod extends Model
+{
+    use HasFactory;
+
+    protected $connection = "mysql";
+    protected $table = "pods";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'short_description',
+        'long_description',
+        'avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'avatar' => null
+    ];
+}
