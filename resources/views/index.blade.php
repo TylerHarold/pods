bde31a18412586ebf6eacbcced7d1093f13924d0
Tyler <tyler@programmer.net>
2021-03-23 21:27:25 -0400

    index + styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,17 @@
 @extends("layouts.master")
 
 @section("body")
+    <br><br><br>
+    <div class="row m-5">
+        <div class="col-md-6 col-sm-12">
+            <h1 class="my-5">❌ Say Goodbye to Unnecessary Group Chats</h1>
+            <p>Make watch parties, set events, post announcements, and chat with your friends in a click of a button.</p>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <div align="right">
+                <img src="/img/illustrations/Text.svg" width="75%" height="auto" />
+            </div>
+        </div>
+    </div>
 
 @endsection
